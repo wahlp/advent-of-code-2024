6228f0785f83abc5e8ce7f53cdd439e80d9299a6
wahlp <wahlp322@gmail.com>
2024-12-06 16:04:45 +0800

    refactor d6p1

--- day6/part1.py
+++ day6/part1.py
@@ -1,3 +1,6 @@
+from itertools import cycle
+
+
 def find_start_pos(data: list[list[int]]):
     for row in range(len(data)):
         for col in range(len(data[row])):
@@ -18,24 +21,28 @@ def get_next_collision(pos: int, obstacles: list[int], forward: bool):
     if forward:
         upcoming = [i for i in obstacles if i > pos]
     else:
-        upcoming = [i for i in obstacles if i < pos]
+        upcoming = list(reversed([i for i in obstacles if i < pos]))
 
     if not upcoming:
         return -1
-    if forward:
-        return upcoming[0]
-    else:
-        return upcoming[-1]
+
+    return upcoming[0]
+
+
+seq = cycle(['up', 'right', 'down', 'left'])
+def direction_iterator():
+    yield next(seq)
 
 
 with open('day6/input.txt') as f:
     data = f.read().splitlines()
 
 pos = find_start_pos(data)
-direction = 'up'
 visited = [pos]
 
 while True:
+    direction = next(direction_iterator())
+
     if direction == 'up':
         path = get_col_as_list(data, pos[1])
         obstacles = get_indexes_of_obstacles(path)
@@ -45,7 +52,6 @@ while True:
             break
         new_pos = (collided_row + 1, pos[1])
         visited.extend([(row, pos[1]) for row in range(pos[0], new_pos[0] - 1, -1)])
-        direction = 'right'
 
     elif direction == 'down':
         path = get_col_as_list(data, pos[1])
@@ -56,7 +62,6 @@ while True:
             break
         new_pos = (collided_row - 1, pos[1])
         visited.extend([(row, pos[1]) for row in range(pos[0], new_pos[0] + 1)])
-        direction = 'left'
 
     elif direction == 'left':
         path = data[pos[0]]
@@ -67,7 +72,6 @@ while True:
             break
         new_pos = (pos[0], collided_col + 1)
         visited.extend([(pos[0], col) for col in range(pos[1], new_pos[1] - 1, -1)])
-        direction = 'up'
 
     elif direction == 'right':
         path = data[pos[0]]
@@ -78,19 +82,19 @@ while True:
             break
         new_pos = (pos[0], collided_col - 1)
         visited.extend([(pos[0], col) for col in range(pos[1], new_pos[1] + 1)])
-        direction = 'down'
 
     pos = new_pos
 
 # debugging
-obstacle_count = sum(row.count('#') for row in data)
-print(f'{obstacle_count = }')
+obstacle_count_bef = sum(row.count('#') for row in data)
+print(f'{obstacle_count_bef = }')
 for coord in set(visited):
     data[coord[0]] = data[coord[0]][:coord[1]] + 'X' + data[coord[0]][coord[1] + 1:]
 for i, row in enumerate(data):
-    print(f'{i:<3} | {row}')
-obstacle_count = sum(row.count('#') for row in data)
-print(f'{obstacle_count = }')
+    print(f'{i+1:<3} | {row}')
+obstacle_count_aft = sum(row.count('#') for row in data)
+print(f'{obstacle_count_aft = }')
+assert obstacle_count_bef == obstacle_count_aft, 'deleted some obstacles during traversal'
 
 total = len(set(visited))
 print(total)
