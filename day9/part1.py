1f4ea5ff3bb1325b5529e19cfe73c1119e2ffc18
wahlp <wahlp322@gmail.com>
2024-12-09 21:53:30 +0800

    solve d9p1

--- day9/part1.py
+++ day9/part1.py
@@ -1,28 +1,24 @@
 with open('day9/input.txt') as f:
     data = f.read().replace('\n', '')
 
+empty_char = None
+
 block_id = 0
-s = ''
+block_expr = []
 for i, char in enumerate(data):
+    num_repeat = int(char)
     if i % 2 == 0:
-        s += str(block_id) * int(char)
+        block_expr.extend([block_id] * num_repeat)
         block_id += 1
     else:
-        s += '.' * int(char)
+        block_expr.extend([empty_char] * num_repeat)
 
-# print(s)
-
-block_expr = list(s)
-while '.' in block_expr:
+while empty_char in block_expr:
     last = block_expr.pop()
-    if last == '.':
+    if last == empty_char:
         continue
-    first_opening = block_expr.index('.')
+    first_opening = block_expr.index(empty_char)
     block_expr[first_opening] = last
 
-# print(''.join(block_expr))
-
-total = 0
-for i, char in enumerate(block_expr):
-    total += i * int(char)
+total = sum(i * int(char) for i, char in enumerate(block_expr))
 print(f'{total = }')
