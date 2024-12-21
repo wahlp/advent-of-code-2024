f9d20a3033ca44b1f83a6a06dbf3731675a432dd
wahlp <wahlp322@gmail.com>
2024-12-21 19:10:06 +0800

    refactor d21p1

--- day21/part1.py
+++ day21/part1.py
@@ -1,9 +1,12 @@
+from functools import cache
+
+
 def read_input(filename: str):
     with open(f'day21/{filename}') as f:
         data = f.read().splitlines()
     return data
 
-
+@cache
 def calc_paths(pos1, pos2, illegal_pos):
     dy = pos1[0] - pos2[0]
     dx = pos1[1] - pos2[1]
@@ -29,12 +32,19 @@ def calc_paths(pos1, pos2, illegal_pos):
         pos2[1] == illegal_pos[1] and pos1[0] == illegal_pos[0]
     )
 
-    paths = []
     if not could_vertically_enter_illegal_space:
-        paths.append(y_component + x_component + 'A')
-    if not could_horizontally_enter_illegal_space:
-        paths.append(x_component + y_component + 'A')
-    return list(set(paths))
+        # <^ results in shorter code later on than ^<
+        if (
+            not could_horizontally_enter_illegal_space 
+            and ('<' in x_component and '^' in y_component)
+        ):
+            path = x_component + y_component + 'A'
+        else:
+            path = y_component + x_component + 'A'
+    elif not could_horizontally_enter_illegal_space:
+        path = x_component + y_component + 'A'
+
+    return path
 
 NUMERIC_KEYPAD = {
     '7': (0, 0),
@@ -49,19 +59,13 @@ NUMERIC_KEYPAD = {
     '0': (3, 1),
     'A': (3, 2),
 }
-
-from itertools import product
 def calc_numeric_button_code_path(code: str):
     pos = NUMERIC_KEYPAD['A']
-    paths = []
+    path = ''
     for c in code:
-        subpaths = calc_paths(pos, NUMERIC_KEYPAD[c], (3, 0))
-        if paths:
-            paths = [''.join(path) for path in product(paths, subpaths)]
-        else:
-            paths = subpaths
+        path += calc_paths(pos, NUMERIC_KEYPAD[c], (3, 0))
         pos = NUMERIC_KEYPAD[c]
-    return list(paths)
+    return path
 
 DIRECTIONAL_KEYPAD = {
     '^': (0, 1),
@@ -72,55 +76,37 @@ DIRECTIONAL_KEYPAD = {
 }
 def calc_directional_button_code_path(code: str):
     pos = DIRECTIONAL_KEYPAD['A']
-    paths = []
+    path = ''
     for c in code:
-        subpaths = calc_paths(pos, DIRECTIONAL_KEYPAD[c], (0, 0))
-        if paths:
-            paths = [''.join(path) for path in product(paths, subpaths)]
-        else:
-            paths = subpaths
+        path += calc_paths(pos, DIRECTIONAL_KEYPAD[c], (0, 0))
         pos = DIRECTIONAL_KEYPAD[c]
-    return paths
+    return path
 
 
-def calc_complexity(button_presses: str, code: str):
-   a = len(button_presses)
-   b = int(''.join(c for c in code if c.isdigit()))
+def calc_complexity(code: str, processed_code: str):
+   a = len(processed_code)
+   b = int(code[:-1])
    return a * b
 
 
-def filter_shortest(a: list):
-    if not a:
-        return []
-    shortest = len(min(a))
-    return [x for x in a if len(x) == shortest]
-
-
-def parse_code(code):
-    codes_lv1 = calc_numeric_button_code_path(code)
-    codes_lv1 = filter_shortest(codes_lv1)
-
-    codes_lv2 = []
-    for code_lv1 in codes_lv1:
-        codes_lv2.extend(calc_directional_button_code_path(code_lv1))
-    codes_lv2 = filter_shortest(codes_lv2)
+def parse_code(code, n):
+    code_layers = [calc_numeric_button_code_path(code)]
+    for i in range(1, n):
+        print(f'calculating directional layer {i}')
+        processed_code = calc_directional_button_code_path(code_layers[-1])
+        code_layers.append(processed_code)
     
-    codes_lv3 = []
-    for code_lv2 in codes_lv2:
-        codes_lv3.extend(calc_directional_button_code_path(code_lv2))
-    codes_lv3 = filter_shortest(codes_lv3)
-    
-    return calc_complexity(codes_lv3[0], code)
+    return calc_complexity(code, processed_code)
 
 
-def main(filename: str):
+def main(filename: str, layers: int):
     data = read_input(filename)
     total = 0
     for code in data:
-        total += parse_code(code)
+        total += parse_code(code, layers)
     return total
     
 
-# r = main('sample.txt')
-r = main('input.txt')
+assert main('sample.txt', 3) == 126384
+r = main('input.txt', 3)
 print(f'answer: {r}')
