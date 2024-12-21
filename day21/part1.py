d4cb34b0d6b486d6355e9ea49a83c663868dadd1
wahlp <wahlp322@gmail.com>
2024-12-21 14:59:12 +0800

    solve d21p1

--- day21/part1.py
+++ day21/part1.py
@@ -1,12 +1,10 @@
-
-
 def read_input(filename: str):
     with open(f'day21/{filename}') as f:
         data = f.read().splitlines()
     return data
 
 
-def calc_path(pos1, pos2, illegal_pos):
+def calc_paths(pos1, pos2, illegal_pos):
     dy = pos1[0] - pos2[0]
     dx = pos1[1] - pos2[1]
 
@@ -24,15 +22,19 @@ def calc_path(pos1, pos2, illegal_pos):
     else:
         x_component = ''
     
-    would_cross_illegal_space = (
+    could_vertically_enter_illegal_space = (
         pos1[1] == illegal_pos[1] and pos2[0] == illegal_pos[0]
-        # avoid vertical into horizontal crossing illegal space
     )
-    if would_cross_illegal_space or ('<' in x_component and '^' in y_component):
-        path = x_component + y_component
-    else:
-        path = y_component + x_component
-    return path
+    could_horizontally_enter_illegal_space = (
+        pos2[1] == illegal_pos[1] and pos1[0] == illegal_pos[0]
+    )
+
+    paths = []
+    if not could_vertically_enter_illegal_space:
+        paths.append(y_component + x_component + 'A')
+    if not could_horizontally_enter_illegal_space:
+        paths.append(x_component + y_component + 'A')
+    return list(set(paths))
 
 NUMERIC_KEYPAD = {
     '7': (0, 0),
@@ -48,13 +50,18 @@ NUMERIC_KEYPAD = {
     'A': (3, 2),
 }
 
+from itertools import product
 def calc_numeric_button_code_path(code: str):
     pos = NUMERIC_KEYPAD['A']
-    path = ''
+    paths = []
     for c in code:
-        path += calc_path(pos, NUMERIC_KEYPAD[c], (3, 0)) + 'A'
+        subpaths = calc_paths(pos, NUMERIC_KEYPAD[c], (3, 0))
+        if paths:
+            paths = [''.join(path) for path in product(paths, subpaths)]
+        else:
+            paths = subpaths
         pos = NUMERIC_KEYPAD[c]
-    return path
+    return list(paths)
 
 DIRECTIONAL_KEYPAD = {
     '^': (0, 1),
@@ -65,22 +72,45 @@ DIRECTIONAL_KEYPAD = {
 }
 def calc_directional_button_code_path(code: str):
     pos = DIRECTIONAL_KEYPAD['A']
-    path = ''
+    paths = []
     for c in code:
-        path += calc_path(pos, DIRECTIONAL_KEYPAD[c], (0, 0)) + 'A'
+        subpaths = calc_paths(pos, DIRECTIONAL_KEYPAD[c], (0, 0))
+        if paths:
+            paths = [''.join(path) for path in product(paths, subpaths)]
+        else:
+            paths = subpaths
         pos = DIRECTIONAL_KEYPAD[c]
-    return path
+    return paths
+
 
 def calc_complexity(button_presses: str, code: str):
    a = len(button_presses)
    b = int(''.join(c for c in code if c.isdigit()))
    return a * b
 
+
+def filter_shortest(a: list):
+    if not a:
+        return []
+    shortest = len(min(a))
+    return [x for x in a if len(x) == shortest]
+
+
 def parse_code(code):
-    directional_code_1 = calc_numeric_button_code_path(code)
-    directional_code_2 = calc_directional_button_code_path(directional_code_1)
-    directional_code_3 = calc_directional_button_code_path(directional_code_2)
-    return calc_complexity(directional_code_3, code)
+    codes_lv1 = calc_numeric_button_code_path(code)
+    codes_lv1 = filter_shortest(codes_lv1)
+
+    codes_lv2 = []
+    for code_lv1 in codes_lv1:
+        codes_lv2.extend(calc_directional_button_code_path(code_lv1))
+    codes_lv2 = filter_shortest(codes_lv2)
+    
+    codes_lv3 = []
+    for code_lv2 in codes_lv2:
+        codes_lv3.extend(calc_directional_button_code_path(code_lv2))
+    codes_lv3 = filter_shortest(codes_lv3)
+    
+    return calc_complexity(codes_lv3[0], code)
 
 
 def main(filename: str):
@@ -91,7 +121,6 @@ def main(filename: str):
     return total
     
 
-
 # r = main('sample.txt')
-# r = main('input.txt')
+r = main('input.txt')
 print(f'answer: {r}')
