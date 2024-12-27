534c8072cba5e6cee6c02af56258e23a5ec81bd3
wahlp <wahlp322@gmail.com>
2024-12-27 22:01:51 +0800

    clean up ruler script

--- scripts/ruler.py
+++ scripts/ruler.py
@@ -1,3 +1,6 @@
+import sys
+from enum import Enum
+
 from common import get_aoc_scripts
 
 
@@ -11,17 +14,28 @@ def measure_size(script):
             comments += 1
     
     return size, comments
-    
 
-def main():
+
+class SortBy(Enum):
+    name = 0
+    size = 1
+    comments = 2
+
+
+def main(sort_by: SortBy = SortBy.name):
     scripts = get_aoc_scripts()
+    data = [(script, *measure_size(script)) for script in scripts]
+    data.sort(key=lambda x: x[sort_by.value])
     
     print(f'{'script':<15} | {'size':>6} | {'comments':>9}')
     print(f'{'-'.ljust(15, '-')}-+-{'-'.rjust(6, '-')}-+-{'-'.rjust(9, '-')}')
-    for script in scripts:
-        size, comments = measure_size(script)
+    for script, size, comments in data:
         print(f'{script:<15} | {size:>6} | {comments:>9}')
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 2:
+        sort_by = SortBy[sys.argv[1]]
+        main(sort_by)
+    else:
+        main()
