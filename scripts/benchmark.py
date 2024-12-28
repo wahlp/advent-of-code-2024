2bc2969b47649897bec645104016b44ea3b95139
wahlp <wahlp322@gmail.com>
2024-12-28 23:35:43 +0800

    refactor benchmark script

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,13 +1,28 @@
+import sys
+from enum import Enum
+
 from codetimer import time_script
 from common import get_aoc_scripts
 
 
-def main():
+class SortBy(Enum):
+    name = 0
+    runtime = 1
+
+def main(sort_by: SortBy = SortBy.name):
     scripts = get_aoc_scripts()
-    for script in scripts:
-        t = time_script(script, ignore_prints=True)
-        print(f'{script:<15} | {t:>9.3f} ms')
+    data = [(script, time_script(script, ignore_prints=True)) for script in scripts]
+    data.sort(key=lambda x: x[sort_by.value])
+
+    print(f'{'script':<15} | {'time':>9}')
+    print(f'{'-'.ljust(15, '-')}-+-{'-'.rjust(9, '-')}')
+    for script, runtime in data:
+        print(f'{script:<15} | {runtime:>9.3f} ms')
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 2:
+        sort_by = SortBy[sys.argv[1]]
+        main(sort_by)
+    else:
+        main()
