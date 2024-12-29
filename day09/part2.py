174c2adfd5f6b649f4aa0f04ff8c3952be65c7f8
wahlp <wahlp322@gmail.com>
2024-12-30 05:45:56 +0800

    refactor d9p2

--- day09/part2.py
+++ day09/part2.py
@@ -1,50 +1,60 @@
-with open('day09/input.txt') as f:
-    data = f.read().replace('\n', '')
+EMPTY_CHAR = None
 
-empty_char = '.'
+def parse_input(filename):
+    with open(f'day09/{filename}') as f:
+        data = f.read().replace('\n', '')
 
-block_id = 0
-block_expr = []
-blocks = {}
-spaces = {}
-for i, num_repeat in enumerate(data):
-    num_repeat = int(num_repeat)
-    if i % 2 == 0:
-        blocks[block_id] = (len(block_expr), num_repeat)
-        block_expr.extend([block_id] * num_repeat)
-        block_id += 1
-    else:
-        spaces[len(block_expr)] = num_repeat
-        block_expr.extend([empty_char] * num_repeat)
+    block_id = 0
+    block_expr = []
+    for i, char in enumerate(data):
+        if not block_expr:
+            span = range(i, i + int(char))
+        else:
+            start = block_expr[-1][0].stop
+            span = range(start, start + int(char))
 
-for block_id in reversed(blocks.keys()):
-    block_loc, block_size = blocks[block_id]
-    # update order after inserting in previous iteration
-    spaces = dict(sorted(spaces.items()))
-
-    space_loc = next(
-        (space_loc for space_loc, space_size in spaces.items() if 0 < block_size <= space_size), -1
-    )
-    if space_loc == -1:
-        continue
-    # dont move to the right
-    if space_loc > block_loc:
-        continue
-    # update block location
-    blocks[block_id] = (space_loc, block_size)
-    # shrink original space
-    space_size = spaces[space_loc]
-    spaces[space_loc] = 0
-    spaces[space_loc + block_size] = space_size - block_size
+        if i % 2 == 0:
+            block_expr.append((span, block_id))
+            block_id += 1
+        else:
+            block_expr.append((span, EMPTY_CHAR))
+    return block_expr
 
 
-s = [empty_char] * len(block_expr)
-for block_id, (block_loc, block_size) in blocks.items():
-    for i in range(block_loc, block_loc + block_size):
-        s[i] = block_id
+def main(filename):
+    block_expr = parse_input(filename)
 
-total = 0
-for i, block_id in enumerate(s):
-    if block_id != empty_char:
-        total += block_id * i
-print(f'{total = }')
+    additions = 0
+    for old_index, (span1, block_id1) in enumerate(reversed(block_expr)):
+        old_index = len(block_expr) - old_index - additions - 1
+        if block_id1 == EMPTY_CHAR:
+            continue
+
+        try:
+            new_index = next(
+                i for i, (span, block_id) in enumerate(block_expr) 
+                if block_id == EMPTY_CHAR and len(span1) <= len(span) 
+            )
+        except StopIteration:
+            continue
+
+        span2 = block_expr[new_index][0]
+        if span2.start > span1.start:
+            # do not move to the right
+            continue
+        # found a legal move
+        block_expr[old_index] = (span1, EMPTY_CHAR)
+        block_expr[new_index] = (range(span2.start, span2.start + len(span1)), block_id1)
+        if (diff := len(span2) - len(span1)) > 0:
+            new_start = span2.start + len(span1)
+            block_expr.insert(new_index + 1, (range(new_start, new_start + diff), EMPTY_CHAR))
+            additions += 1
+
+    # full_block_expr = ''.join(str(block_id) if block_id != EMPTY_CHAR else '.' for span, block_id in block_expr for _ in span)
+    total = sum(sum(span) * block_id for span, block_id in block_expr if block_id != EMPTY_CHAR)
+    return total
+
+
+assert main('sample.txt') == 2858
+r = main('input.txt')
+print(f'answer = {r}')
