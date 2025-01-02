0c0e7ccbe4af4f8d1bf58131446c147b11bdc1ef
wahlp <wahlp322@gmail.com>
2025-01-02 10:23:50 +0800

    refactor d6p2

--- day06/part2.py
+++ day06/part2.py
@@ -1,196 +1,138 @@
-from collections import defaultdict
-from copy import deepcopy
+from typing import NamedTuple, Self
 
 
-def find_start_pos(data: list[list[int]]):
-    for row in range(len(data)):
-        for col in range(len(data[row])):
-            if data[row][col] == '^':
-                return row, col
-    raise Exception('bruh')
+class Position(NamedTuple):
+    y: int
+    x: int
 
-def get_col_as_list(data: list[list[str]], col: int):
-    return [row[col] for row in data]
+    def __add__(self, other: Self) -> Self:
+        return type(self)(self.y + other.y, self.x + other.x)
+    
+    def __sub__(self, other: Self) -> Self:
+        return type(self)(self.y - other.y, self.x - other.x)
 
-def get_indexes_of_obstacles(path: list):
-    return [i for i, x in enumerate(path) if x == '#']
+    def value(self, data: list[list]):
+        return data[self.y][self.x]
 
-def get_next_collision(pos: int, obstacles: list[int], forward: bool):
-    if forward:
-        upcoming = [i for i in obstacles if i > pos]
-    else:
-        upcoming = list(reversed([i for i in obstacles if i < pos]))
-
-    if not upcoming:
-        return -1
-
-    return upcoming[0]
-
-def step_direction(direction: str, n: int = 1):
-    seq = ['up', 'right', 'down', 'left']
-    i = (seq.index(direction) + n) % len(seq)
-    return seq[i]
-
-def find_collision_up(data, pos):
-    path = get_col_as_list(data, pos[1])
-    obstacles = get_indexes_of_obstacles(path)
-    collided_row = get_next_collision(pos[0], obstacles, forward=False)
-    return collided_row
-
-def find_collision_down(data, pos):
-    path = get_col_as_list(data, pos[1])
-    obstacles = get_indexes_of_obstacles(path)
-    collided_row = get_next_collision(pos[0], obstacles, forward=True)
-    return collided_row
-
-def find_collision_left(data, pos):
-    path = data[pos[0]]
-    obstacles = get_indexes_of_obstacles(path)
-    collided_col = get_next_collision(pos[1], obstacles, forward=False)
-    return collided_col
-
-def find_collision_right(data, pos):
-    path = data[pos[0]]
-    obstacles = get_indexes_of_obstacles(path)
-    collided_col = get_next_collision(pos[1], obstacles, forward=True)
-    return collided_col
-
-def calculate_theoretical_collision(data, cell, direction):
-    if direction == 'up':
-        collided_row = find_collision_up(data, cell)
-        collision = (collided_row, cell[1])
-        new_pos = (collision[0] + 1, collision[1])
-    elif direction == 'down':
-        collided_row = find_collision_down(data, cell)
-        collision = (collided_row, cell[1])
-        new_pos = (collision[0] - 1, collision[1])
-    elif direction == 'left':
-        collided_col = find_collision_left(data, cell)
-        collision = (cell[0], collided_col)
-        new_pos = (collision[0], collision[1] + 1)
-    elif direction == 'right':
-        collided_col = find_collision_right(data, cell)
-        collision = (cell[0], collided_col)
-        new_pos = (collision[0], collision[1] - 1)
-    return collision, new_pos
+    def is_oob(self, data: list[list]):
+        return not (0 <= self.y < len(data) and 0 <= self.x < len(data[0]))
 
 
+DIRECTIONS = [
+    Position(-1, 0), 
+    Position(0, 1), 
+    Position(1, 0), 
+    Position(0, -1)
+]
+def step_direction(direction: Position):
+    i = (DIRECTIONS.index(direction) + 1) % 4
+    return DIRECTIONS[i]
 
-def run(data, pos, direction) -> bool:
-    visited = defaultdict(list)
-    collided = defaultdict(list)
 
-    is_pos_oob = False
+def parse_input(filename: str):
+    with open(f"day06/{filename}") as f:
+        return f.read().splitlines()
+
+
+def get_jump_location(grid: list[list[str]], pos: Position, direction: Position):
+    while pos.value(grid) != "#":
+        pos += direction
+
+        if pos.is_oob(grid):
+            return (pos, direction)
+
+    pos -= direction
+    direction = step_direction(direction)
+    return (pos, direction)
+
+
+def calc_jumps(grid: list[list[str]]):
+    jumps: dict[tuple[Position, Position], tuple[Position, Position]] = {}
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            pos = Position(y, x)
+            for direction in DIRECTIONS:
+                if pos.value(grid) == '#':
+                    continue
+                jumps[(Position(y, x), direction)] = get_jump_location(grid, Position(y, x), direction)
+    return jumps
+
+
+def jump_into_block(block_patch: Position, direction: Position):
+    return (block_patch - direction, step_direction(direction))
+
+
+def jump(
+    pos: Position, 
+    direction: Position, 
+    block: Position, 
+    jump_map: dict[tuple[Position, Position], tuple[Position, Position]], 
+):
+    dest = jump_map[(pos, direction)]
+    if block is not None and dest is not None:
+        fpos, _ = dest
+        if (
+            fpos.x == block.x and min(pos.y, fpos.y) <= block.y <= max(pos.y, fpos.y)
+            or fpos.y == block.y and min(pos.x, fpos.x) <= block.x <= max(pos.x, fpos.x)
+        ):
+            return jump_into_block(block, direction)
+    return dest
+
+
+def get_full_path(grid: list[list[str]], start_pos: Position, direction: Position):
+    pos = start_pos
+    visited = set()
     while True:
-        if direction == 'up':
-            collided_row = find_collision_up(data, pos)
-            if collided_row == -1:
-                just_visited = [(row, pos[1]) for row in range(pos[0], -1, -1)]
-                is_pos_oob = True
-            else:
-                new_pos = (collided_row + 1, pos[1])
-                just_visited = [(row, pos[1]) for row in range(pos[0], new_pos[0] - 1, -1)]
-                obstacle_pos = (collided_row, pos[1])
-                collided[obstacle_pos].append(direction)
-                if len(collided[obstacle_pos]) != len(set(collided[obstacle_pos])):
-                    return visited, True
+        # visited.add((pos, direction))
+        visited.add(pos)
 
-        elif direction == 'down':
-            collided_row = find_collision_down(data, pos)
-            if collided_row == -1:
-                just_visited = [(row, pos[1]) for row in range(pos[0], len(data))]
-                is_pos_oob = True
-            else:
-                new_pos = (collided_row - 1, pos[1])
-                just_visited = [(row, pos[1]) for row in range(pos[0], new_pos[0] + 1)]
-                obstacle_pos = (collided_row, pos[1])
-                collided[obstacle_pos].append(direction)
-                if len(collided[obstacle_pos]) != len(set(collided[obstacle_pos])):
-                    return visited, True
+        pos += direction
+        if pos.is_oob(grid):
+            break
+        if pos.value(grid) == "#":
+            pos -= direction
+            direction = step_direction(direction)
 
-        elif direction == 'left':
-            collided_col = find_collision_left(data, pos)
-            if collided_col == -1:
-                just_visited = [(pos[0], col) for col in range(pos[1], -1, -1)]
-                is_pos_oob = True
-            else:
-                new_pos = (pos[0], collided_col + 1)
-                just_visited = [(pos[0], col) for col in range(pos[1], new_pos[1] - 1, -1)]
-                obstacle_pos = (pos[0], collided_col)
-                collided[obstacle_pos].append(direction)
-                if len(collided[obstacle_pos]) != len(set(collided[obstacle_pos])):
-                    return visited, True
-
-        elif direction == 'right':
-            collided_col = find_collision_right(data, pos)
-            if collided_col == -1:
-                just_visited = [(pos[0], col) for col in range(pos[1], len(data[0]))]
-                is_pos_oob = True
-            else:
-                new_pos = (pos[0], collided_col - 1)
-                just_visited = [(pos[0], col) for col in range(pos[1], new_pos[1] + 1)]
-                obstacle_pos = (pos[0], collided_col)
-                collided[obstacle_pos].append(direction)
-                if len(collided[obstacle_pos]) != len(set(collided[obstacle_pos])):
-                    return visited, True
-
-        for cell in just_visited:
-            visited[cell].append(direction)
-
-        if is_pos_oob:
-            return visited, False
-
-        pos = new_pos
-        direction = step_direction(direction)
+    return visited
 
 
-def replace_at_coord(grid, pos, value):
-    y, x = pos
-    grid[y] = grid[y][:x] + value + grid[y][x + 1:]
+def path_loops_with_patch(grid: list[list[str]], block: Position, start_pos: Position, direction: Position, jump_map):
+    pos = start_pos
+    visited = set()
+    while True:
+        if pos.is_oob(grid):
+            return False
+        
+        pos, direction = jump(pos, direction, block, jump_map)
+        if direction is None:
+            return False
+
+        if (pos, direction) in visited:
+            return True
+
+        visited.add((pos, direction))
 
 
-with open('day06/input.txt') as f:
-    data = f.read().splitlines()
+def main(filename: str):
+    grid = parse_input(filename)
 
-start_pos = find_start_pos(data)
-visited, is_looping = run(data, start_pos, 'up')
+    start_pos = next(
+        Position(y, x) for y, row in enumerate(grid) for x, c in enumerate(row) if c == "^"
+    )
 
-candidates = []
-for cell in visited:
-    if cell == start_pos:
-        continue
-    # make alternate version of map with a new obstacle
-    alt_data = deepcopy(data)
-    replace_at_coord(alt_data, cell, '#')
-    # run it to see if it loops
-    _, is_looping = run(alt_data, start_pos, 'up')
-    if is_looping:
-        candidates.append(cell)
+    path = get_full_path(grid, start_pos, Position(-1, 0))
+    # return len(path)
+    # print(len(path))
+
+    valid_blocks = set()
+    jumps = calc_jumps(grid)
+    for block in path:
+        if block != start_pos and path_loops_with_patch(grid, block, start_pos, Position(-1, 0), jumps):
+            valid_blocks.add(block)
+
+    return len(valid_blocks)
 
 
-# debugging
-obstacle_count_bef = sum(row.count('#') for row in data)
-print(f'{obstacle_count_bef = }')
-for coord, passed_directions in visited.items():
-    if len(passed_directions) == 1:
-        if passed_directions[0] == 'left':
-            c = '<'
-        elif passed_directions[0] == 'right':
-            c = '>'
-        elif passed_directions[0] == 'up':
-            c = '^'
-        else:
-            c = 'v'
-    else:
-        c = '+'
-    replace_at_coord(data, coord, c)
-for coord in candidates:
-    replace_at_coord(data, coord, 'O')
-for i, row in enumerate(data):
-    print(f'{i+1:<3} | {row}')
-obstacle_count_aft = sum(row.count('#') for row in data)
-print(f'{obstacle_count_aft = }')
-assert obstacle_count_bef == obstacle_count_aft, 'deleted some obstacles during traversal'
-
-total = len(candidates) 
-print(f'answer: {total}')
+# r = main('sample.txt')
+r = main('input.txt')
+print(f'answer = {r}')
