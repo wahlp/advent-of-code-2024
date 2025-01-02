e19d4e03038035eb7242524d6956eddaab0fcf13
wahlp <wahlp322@gmail.com>
2025-01-02 10:39:18 +0800

    speed up d6p2

--- day06/part2.py
+++ day06/part2.py
@@ -11,12 +11,6 @@ class Position(NamedTuple):
     def __sub__(self, other: Self) -> Self:
         return type(self)(self.y - other.y, self.x - other.x)
 
-    def value(self, data: list[list]):
-        return data[self.y][self.x]
-
-    def is_oob(self, data: list[list]):
-        return not (0 <= self.y < len(data) and 0 <= self.x < len(data[0]))
-
 
 DIRECTIONS = [
     Position(-1, 0), 
@@ -35,10 +29,11 @@ def parse_input(filename: str):
 
 
 def get_jump_location(grid: list[list[str]], pos: Position, direction: Position):
-    while pos.value(grid) != "#":
+    while grid[pos.y][pos.x] != "#":
         pos += direction
 
-        if pos.is_oob(grid):
+        # oob
+        if not (0 <= pos.y < len(grid) and 0 <= pos.x < len(grid[0])):
             return (pos, direction)
 
     pos -= direction
@@ -52,14 +47,14 @@ def calc_jumps(grid: list[list[str]]):
         for x in range(len(grid[0])):
             pos = Position(y, x)
             for direction in DIRECTIONS:
-                if pos.value(grid) == '#':
+                if grid[pos.y][pos.x] == '#':
                     continue
                 jumps[(Position(y, x), direction)] = get_jump_location(grid, Position(y, x), direction)
     return jumps
 
 
-def jump_into_block(block_patch: Position, direction: Position):
-    return (block_patch - direction, step_direction(direction))
+def new_jump_using_block(block: Position, direction: Position):
+    return (block - direction, step_direction(direction))
 
 
 def jump(
@@ -68,41 +63,37 @@ def jump(
     block: Position, 
     jump_map: dict[tuple[Position, Position], tuple[Position, Position]], 
 ):
-    dest = jump_map[(pos, direction)]
-    if block is not None and dest is not None:
-        fpos, _ = dest
+    jump = jump_map.get((pos, direction))
+    if jump is not None:
+        fpos, _ = jump
         if (
             fpos.x == block.x and min(pos.y, fpos.y) <= block.y <= max(pos.y, fpos.y)
             or fpos.y == block.y and min(pos.x, fpos.x) <= block.x <= max(pos.x, fpos.x)
         ):
-            return jump_into_block(block, direction)
-    return dest
-
+            return new_jump_using_block(block, direction)
+        return jump
+    return (None, None)
 
 def get_full_path(grid: list[list[str]], start_pos: Position, direction: Position):
     pos = start_pos
     visited = set()
     while True:
-        # visited.add((pos, direction))
         visited.add(pos)
 
         pos += direction
-        if pos.is_oob(grid):
+        if not (0 <= pos.y < len(grid) and 0 <= pos.x < len(grid[0])):
             break
-        if pos.value(grid) == "#":
+        if grid[pos.y][pos.x] == "#":
             pos -= direction
             direction = step_direction(direction)
 
     return visited
 
 
-def path_loops_with_patch(grid: list[list[str]], block: Position, start_pos: Position, direction: Position, jump_map):
+def path_loops_with_patch(block: Position, start_pos: Position, direction: Position, jump_map):
     pos = start_pos
     visited = set()
-    while True:
-        if pos.is_oob(grid):
-            return False
-        
+    while True:        
         pos, direction = jump(pos, direction, block, jump_map)
         if direction is None:
             return False
@@ -121,13 +112,11 @@ def main(filename: str):
     )
 
     path = get_full_path(grid, start_pos, Position(-1, 0))
-    # return len(path)
-    # print(len(path))
 
     valid_blocks = set()
     jumps = calc_jumps(grid)
     for block in path:
-        if block != start_pos and path_loops_with_patch(grid, block, start_pos, Position(-1, 0), jumps):
+        if block != start_pos and path_loops_with_patch(block, start_pos, Position(-1, 0), jumps):
             valid_blocks.add(block)
 
     return len(valid_blocks)
